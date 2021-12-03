c24d4902a7d93b0ab26b39ffd3c56438d5b13388
Filipe Nogueira <84330960+Suricat0Br@users.noreply.github.com>
2021-12-03 16:19:41 -0300

    alterando tudo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,26 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav-Menu'),
+    navToggle = document.getElementById('nav-Toggle'),
+    navClose = document.getElementById('nav-Close');
 
-/*==================== MENU SHOW =============================*/
-
-/*==================== MENU HIDDEN ===========================*/
-
+if (navToggle) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('showMenu');
+    })
+}
+if (navClose) {
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('showMenu');
+    })
+}
 /*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.navLink')
 
+function linkAction() {
+    const navMenu = document.getElementById('nav-Menu')
+    navMenu.classList.remove('showMenu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== ACCORDION SKILLS ======================*/
 
 /*==================== QUALIFICATION TABS ====================*/
@@ -16,8 +31,8 @@
 
 /*==================== SCROLL SECTIONS ACTIVE LINK ===========*/
 
-/*==================== CHANGE BACKGROUND HEADER ==============*/ 
+/*==================== CHANGE BACKGROUND HEADER ==============*/
 
-/*==================== SHOW SCROLL UP ========================*/ 
+/*==================== SHOW SCROLL UP ========================*/
 
-/*==================== DARK LIGHT THEME ======================*/ 
+/*==================== DARK LIGHT THEME ======================*/
