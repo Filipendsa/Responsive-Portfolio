34def3fef840695315025c51f346427f6b190102
FilipeNdsa <84330960+Filipendsa@users.noreply.github.com>
2023-04-17 18:15:06 -0300

    more sections

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,10 +22,67 @@ function linkAction() {
 }
 navLink.forEach(n => n.addEventListener('click', linkAction))
 /*==================== ACCORDION SKILLS ======================*/
+const skillsContent = document.getElementsByClassName('skillsContent');
+const skillsHeader = document.querySelectorAll('#skillsHeader');
+
+function toggleSkills() {
+  let itemClass = this.parentNode.className;
+  for (let i = 0; i < skillsContent.length; i++) {
+    skillsContent[i].className = 'skillsContent skillsClose';
+  }
+  if (itemClass === 'skillsContent skillsClose') {
+    this.parentNode.className = 'skillsContent skillsOpen';
+  }
+}
+
+skillsHeader.forEach((el) => {
+  el.addEventListener('click', toggleSkills);
+});
 
 /*==================== QUALIFICATION TABS ====================*/
 
+const tabs = document.querySelectorAll('[data-target]');
+const tabsContents = document.querySelectorAll('[data-content]');
+
+tabs.forEach(tab =>{
+  tab.addEventListener('click', ()=>{
+    const target = document.querySelector(tab.dataset.target)
+
+    tabsContents.forEach(tabsContent => {
+      tabsContent.classList.remove('qualificationActive')
+    })
+    target.classList.add('qualificationActive')
+
+    tabs.forEach(tab => {
+      tab.classList.remove('qualificationActive')
+    })
+
+    tab.classList.add('qualificationActive')
+  })
+})
+
 /*==================== SERVICES MODAL ========================*/
+
+const modalViews = document.querySelectorAll('.servicesModal')
+const modalBtn = document.querySelectorAll('.servicesButton')
+const modalCloses = document.querySelectorAll('.servicesModalClose')
+
+let modal = function(modalClick) {
+    modalViews[modalClick].classList.add('activeModal')
+}
+
+modalBtn.forEach((modalBtn,i) => {
+  modalBtn.addEventListener('click', () =>{
+    modal(i)
+  })
+});
+modalCloses.forEach((modalClose) => {
+  modalClose.addEventListener('click', () =>{
+    modalViews.forEach(modalView => {
+      modalView.classList.remove('activeModal')
+    });
+  })
+});
 
 /*==================== PORTFOLIO SWIPER  =====================*/
 
