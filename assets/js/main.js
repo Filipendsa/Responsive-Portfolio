a22c19f18ab51d0a70ea2493638470867d116a80
FilipeNdsa <84330960+Filipendsa@users.noreply.github.com>
2023-04-18 13:18:19 -0300

    More details

--- assets/js/main.js
+++ assets/js/main.js
@@ -86,10 +86,77 @@ modalCloses.forEach((modalClose) => {
 
 /*==================== PORTFOLIO SWIPER  =====================*/
 
+let swiper = new Swiper('.portfolioContainer', {
+  cssMode: true,
+  loop:true,
+  navigation:{
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination:{
+    el: '.swiper-pagination',
+    clickable:true,
+  },
+});
+
 /*==================== SCROLL SECTIONS ACTIVE LINK ===========*/
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive(){
+    const scrollY = window.pageYOffset
+
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight
+        const sectionTop = current.offsetTop - 50;
+        sectionId = current.getAttribute('id')
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight){
+            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.add('active-link')
+        }else{
+            document.querySelector('.navMenu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+window.addEventListener('scroll', scrollActive)
+
 
 /*==================== CHANGE BACKGROUND HEADER ==============*/
 
+function scrollHeader(){
+  const nav = document.getElementById('header')
+  if(this.scrollY >= 80) nav.classList.add('scroll-header'); else nav.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
+
 /*==================== SHOW SCROLL UP ========================*/
 
+function scrollUp(){
+  const scrollUp = document.getElementById('scroll-up');
+  if(this.scrollY >= 560) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
+
 /*==================== DARK LIGHT THEME ======================*/
+
+const themeButton = document.getElementById('theme-button')
+const darkTheme = 'dark-theme'
+const iconTheme = 'uil-sun'
+
+const selectedTheme = localStorage.getItem('selected-theme')
+const selectedIcon = localStorage.getItem('selected-icon')
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light'
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? 'uil-moon' : 'uil-sun'
+
+if (selectedTheme) {
+  document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme)
+  themeButton.classList[selectedIcon === 'uil-moon' ? 'add' : 'remove'](iconTheme)
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme)
+    themeButton.classList.toggle(iconTheme)
+    localStorage.setItem('selected-theme', getCurrentTheme())
+    localStorage.setItem('selected-icon', getCurrentIcon())
+})
